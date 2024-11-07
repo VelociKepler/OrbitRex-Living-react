a5e5390c8c72d333b1f2bd7dd96d74aad0aaf73a
Kittipun2710 <new565677@gmail.com>
2024-11-07 16:04:50 +0700

    Create a homepage

---
+++ src/components/Homepage/NavbarHome.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavbarHome() {
+  return (
+    <nav className='w-full h-20 flex items-center justify-around z-10 text-white'>
+      <div className='text-3xl font-bold'>OrbitRex Living</div>
+      <ul className='flex gap-10'>
+        <Link to='/'><li>HOME</li></Link>
+        <Link to='/shop'><li>SHOP</li></Link>
+        <Link to=''><li>CONTACT</li></Link>
+        <Link to=''><li>SERVICES</li></Link>
+      </ul>
+      <div className='relative'>
+        <input type="text" className='px-5 py-2 rounded-full pr-10 text-black outline-none' placeholder='Search' />
+        <button className='absolute top-1/2 right-14 transform -translate-y-2.5 text-gray-300 px-5 rounded-full'><i className="fa-solid fa-magnifying-glass"></i></button>
+        <button><i className="fa-solid fa-circle-user text-3xl ml-7 translate-y-1"></i></button>
+      </div>
+    </nav>
+  )
+}
+
+export default NavbarHome
+
---
+++ src/pages/Home/MainHome.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Image1 from '../../images/Homepage/chair.png'
+import Image2 from '../../images/Homepage/sofa.png'
+import Image3 from '../../images/Homepage/table.png'
+import Image4 from '../../images/Homepage/chair2.jpg'
+import { Link } from 'react-router-dom'
+
+function MainHome() {
+    return (
+
+        // Desktop
+        <div className='lg:-translate-y-20'>
+
+            {/* Main Section 1 */}
+            <div className='my-10'>
+                <div className='h-70 flex justify-center gap-36 my-10 mx-20'>
+                    <div className='my-10 py-14'>
+                        <h1 className='text-2xl font-bold'>Shop <br />by categories</h1>
+                        <button className='font-bold mt-24'>ALL CATEGORIES <i className="fa-duotone fa-solid fa-play text-sm"></i></button>
+                    </div>
+                    <Link to='' className='my-10 h-60'>
+                        <img src={Image1} alt="" className='h-52 py-10' />
+                        <h1 className='text-2xl font-bold'>Chair</h1>
+                    </Link>
+                    <Link to='' className='my-10 h-60'>
+                        <img src={Image2} alt="" className='h-52 py-12' />
+                        <h1 className='text-2xl font-bold'>Sofa</h1>
+                    </Link>
+                    <Link to='' className='my-10 h-60'>
+                        <img src={Image3} alt="" className='h-52 py-10' />
+                        <h1 className='text-2xl font-bold'>Table</h1>
+                    </Link>
+                </div>
+            </div>
+
+            {/* Main Section 2 */}
+            <div>
+                <div className='mx-72'>
+                    <h1 className='text-3xl font-bold'>New Arrivals</h1>
+                    <h1 className='h-0.5 w-44 bg-orange-500 mb-10'></h1>
+
+                    <div className='flex justify-center'>
+                        <>
+                        <img src={Image4} alt="" className='w-80'/>
+                        <h1 className='text-2xl font-bold text-white relative translate-y-3/4 -translate-x-72'>BRAND <br />xx,xxx บาท</h1>
+                        </>
+                        <>
+                        <img src={Image4} alt="" className='w-80'/>
+                        <h1 className='text-2xl font-bold text-white relative translate-y-3/4 -translate-x-72'>BRAND <br />xx,xxx บาท</h1>
+                        </>
+                        <>
+                        <img src={Image4} alt="" className='w-80'/>
+                        <h1 className='text-2xl font-bold text-white relative translate-y-3/4 -translate-x-72'>BRAND <br />xx,xxx บาท</h1>
+                        </>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default MainHome
