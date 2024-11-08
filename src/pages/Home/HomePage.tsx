7e45c9414431140fd72cc70e79fe1b05b8b4c657
Kittipun2710 <new565677@gmail.com>
2024-11-08 14:39:05 +0700

    Edit page section 3,4

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -16,7 +16,7 @@ function HomePage() {
             <NavbarHome />
             <div className='relative -translate-y-20 -z-10 flex'>
                 <img src={Image} alt="" className='w-full h-screen object-cover' />
-                <div className={`bg-black bg-opacity-50 absolute top-80 left-40 p-10 py-10 rounded-xl transition-all duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0 translate-y-10'}`}>
+                <div className={`bg-black bg-opacity-50 absolute top-80 left-40 p-10 rounded-xl transition-all duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0 translate-y-10'}`}>
                     <h1 className='text-6xl font-bold text-white'>Furniture, decor,<br />and beyond</h1>
                     <button className='bg-orange-500 text-white px-5 py-2 rounded-full mt-5'>Shop</button>
                 </div>
