7069f75df5ee57f18cd53ced74e1767e4b108911
Chatpong Deepet <chatpond@outlook.com>
2024-11-13 18:39:20 +0700

    set item center

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,13 +14,13 @@ const Login = () => {
   return (
     <>
       {isSignIn ? (
-        <div className="grid grid-cols-2 h-fit border max-w-screen-lg mx-auto">
-          <div className="items-center justify-center w-full px-4 py-2 border-r-2 text-center">
+        <div className="grid grid-cols-2 h-fit max-w-screen-lg mx-auto h-screen">
+          <div className="items-center justify-center px-4 py-2 border-r-2 text-center my-auto">
             <h1 className="font-bold mb-4">Sign in</h1>
-            <div className="w-">
+            <div>
               {/* Line Login Button */}
               <Button>
-                <FacebookIcon></FacebookIcon>
+                <FacebookIcon fontSize="medium"></FacebookIcon>
               </Button>
               {/* Google Login Button */}
               <Button>
@@ -31,7 +31,9 @@ const Login = () => {
                 <GitHubIcon></GitHubIcon>
               </Button>
             </div>
-            <Divider className="text-gray-300">or</Divider>
+            <div className="flex justify-center items-center mx-auto w-full text-center h-16">
+              <Divider className="text-gray-400 w-48">or</Divider>
+            </div>
             <div className="max-w-screen-lg mx-auto">
               <form
                 className="flex flex-col max-w-[300px] mx-auto text-center"
@@ -39,7 +41,7 @@ const Login = () => {
               >
                 <Box
                   component="form"
-                  sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+                  sx={{ "& > :not(style)": { m: 1, width: "35ch" } }}
                   noValidate
                   autoComplete="off"
                 >
@@ -56,40 +58,37 @@ const Login = () => {
                     autoComplete="current-password"
                   />
                   <Button variant="contained" color="secondary">
-                    Contained
+                    Sign in
                   </Button>
                 </Box>
-                <button className="primary-bnt">Sign-in</button>
               </form>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center h-full px-8">
-            <h1>Slogan</h1>
+          <div className="flex flex-col justify-center items-center px-8">
+            <h1>OrbitRex Living</h1>
             <p className="text-center my-5">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusantium asperiores assumenda cupiditate eius ipsum minima
-              mollitia nulla perferendis quam, unde?
+              "Style Your Space, Simplify Your Life.
             </p>
+            <p>Discover Furniture That Feels Like Home."</p>
             <button className="outline-bnt" onClick={() => setIsSignIn(false)}>
               Sign-up
             </button>
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-2 h-fit border max-w-screen-lg mx-auto">
+        <div className="grid grid-cols-2 h-screen max-w-screen-lg mx-auto ">
           <div className="flex flex-col justify-center items-center h-full px-8">
             <h1 className="font-bold">Sign Up</h1>
             <p className="text-center my-5">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusantium asperiores assumenda cupiditate eius ipsum minima
-              mollitia nulla perferendis quam, unde?
+              "Style Your Space, Simplify Your Life.
             </p>
+            <p>Discover Furniture That Feels Like Home."</p>
             <button className="outline-bnt" onClick={() => setIsSignIn(true)}>
               Sign-in
             </button>
           </div>
-          <div className="items-center justify-center w-full px-4 py-2 text-center">
-            <div className="max-w-screen-lg mx-auto">
+          <div className="items-center justify-center w-full px-4 py-2 text-center my-auto">
+            <div className="max-w-screen-lg mx-auto border-l-2">
               <form
                 className="flex flex-col max-w-[300px] mx-auto"
                 onSubmit={handleSubmit}
@@ -119,7 +118,7 @@ const Login = () => {
                     autoComplete="current-password"
                   />
                   <Button variant="contained" color="secondary">
-                    Contained
+                    Sign up
                   </Button>
                 </Box>
               </form>
