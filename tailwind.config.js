ddb44f8cfb0f55109fa418c3a9c065a0bea15d5f
Chatpong Deepet <chatpond@outlook.com>
2024-11-18 22:41:01 +0700

    Update Login page css animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login-bg-img': "url('/public/images/login-signin-img.png')",
+      }
+    },
   },
   plugins: [],
 }
