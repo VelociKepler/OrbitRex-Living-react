65bc5076f9bdee6f33577296f9df0381953fe969
Chatpong Deepet <chatpond@outlook.com>
2024-12-12 14:44:21 +0700

    Connect login api

--- src/App.tsx
+++ src/App.tsx
@@ -7,46 +7,50 @@ import UserProfilePage from "./pages/UserProfile/UserProfilePage.tsx";
 import ChatWidget from "./components/ChatWidget.tsx";
 import Contact from "./pages/Contact/Contact.tsx";
 import ProductScreen from "./pages/products/ProductScreen.tsx";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Router>
-      <ChatWidget />
-      <Routes>
-        <Route
-          path = "/"
-          element = {<HomePage />}
-        />
-        <Route
-          path = "/login"
-          element = {<Login />}
-        />
-        <Route
-          path = "/contact"
-          element = {<Contact />}
-        />
-        <Route
-          path = "/profile"
-          element = {<UserProfilePage />}
-        />
-        <Route
-          path = "/profile/:id"
-          element = {<UserProfilePage />}
-        />
-        <Route
-          path = "/admin"
-          element = {<AdminPage />}
-        />
-        <Route
-          path = "/products"
-          element = {<ProductPage />}
-        />
-        <Route
-          path = "/products/:id"
-          element = {<ProductScreen />}
-        />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+      <Router>
+        <ChatWidget />
+        <Routes>
+          <Route
+            path = "/"
+            element = {<HomePage />}
+          />
+          <Route
+            path = "/login"
+            element = {<Login />}
+          />
+          <Route
+            path = "/contact"
+            element = {<Contact />}
+          />
+          <Route
+            path = "/profile"
+            element = {<UserProfilePage />}
+          />
+          <Route
+            path = "/profile/:id"
+            element = {<UserProfilePage />}
+          />
+          <Route
+            path = "/admin"
+            element = {<AdminPage />}
+          />
+          <Route
+            path = "/products"
+            element = {<ProductPage />}
+          />
+          <Route
+            path = "/products/:id"
+            element = {<ProductScreen />}
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
